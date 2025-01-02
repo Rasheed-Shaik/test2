c4e81b10db272b9c07de1ed86820fcc01c6db567
Rasheed Shaik <rasheedshaik@Rasheeds-MacBook-Air.local>
2025-01-02 12:06:57 -0800

    test

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+
 import streamlit as st
+import logging
+
+# Get the Streamlit logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG) 
 print("Reached here - Imports done")
 st.write('hello')
